48835e17d3e34c0285307153d8f4196f27c4b09d
Intellix7 <yac.handsport@gmail.com>
2024-12-23 15:28:36 +0100

    Fixed the split function
    + a new print_fields function was added to test it

--- definitions.h
+++ definitions.h
@@ -25,26 +25,51 @@ void hello(){
     printf("Hello\n");
 }
 
+// Prototypes 
+
+// ** split() ** 
+// split takes 2 parameters : 
+//  - char sep which is the separator
+//  - char * txt which is the string of characters that we want to split
+// It returns a t_field * object that contains the list 
+// of the different fields, including the key at the start
+
 t_field * split(char sep, char * txt);
+
+// ** print_fields() **
+// It... prints... the fields...
+ 
+void print_fields(t_field * field_table);
+
+
+// ----------------------------------------------------------------------
+// Actual function declarations
+
 
 t_field * split(char sep, char * txt){
     int l = strlen(txt);
-    printf("%d\n", l);
+    // printf("%d\n", l);
     t_field * field_table = malloc(sizeof(t_field) * MAXFIELDS);
-    int k = 0, i = 0;
-    int j = 0;
+    int k = 0, i = 0, j = 0;
     while ((i < l) && (k < MAXFIELDS)) {
-        printf("%d\n", i);
-        while (txt[i] != sep){
-            printf("%c, %d", txt[i], i);
+        // printf("%d\n", i);
+        while ((txt[i] != sep) && (txt[i] != '\0')){
+            // printf("%c, %d\n", txt[i], i);
             field_table[k][j] = txt[i];
             j++;
             i++;
         }
         k++;
-        j = 0;
         i++;
+        j = 0;
     }
     return field_table;
+}
+
+void print_fields(t_field * field_table){
+    printf("key : %s\n", field_table[0]);
+    for (int i = 1; i < MAXFIELDS-1 ; i++){
+        printf("value %d : %s\n", i, field_table[i]);
+    }
 }
 
--- test.c
+++ test.c
@@ -8,6 +8,8 @@ int main() {
     // printf("%c", a);
 
     t_field * field_table;
-    char * t = "hello:world";
+    char * t = "mot:anagramme 1:anagramme 2:anagramme 3:anagramme 4:anagramme 5:anagramme 6:anagramme 7:anagramme 8:anagramme 9:anagramme 10:anagramme 11:anagramme 12:anagramme 13";
     field_table = split(':', t);
+
+    print_fields(field_table);
 }
