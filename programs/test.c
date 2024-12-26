695ad6a875696429d5079756dd956c366d46064e
Intellix7 <yac.handsport@gmail.com>
2024-12-26 08:33:43 +0100

    Program 1 (V1) officially done. What we did : - Fixed the split function (again) because it was picking up the '\n' character at the end of the line, causing an index offset - Fixed the print_tuples function (some segfaults here and there) and changed the declaration to include a third argument, being the word to find (the control loop and the scanf call is now handled outside) - Wrote the final lines of the main.c file and added some comments where due. - Wrote a bunch of tests to make sure everything works fine - Rearranged the repo to make it cleaner
    What might be done (eventually) :
    - Change the type declarations to allocate less memory (use malloc and realloc while keeping track of the size of the dictionnary)
    - Implement an insert sort while filling the dictionnary and implement a binary search (Maybe++)

---
+++ programs/test.c
@@ -0,0 +1,30 @@
+#include "definitions.h"
+
+int main() {
+    
+    // char t[5][5];
+    // t[4][1] = 'f';
+    // char a = t[4][1];
+    // printf("%c", a);
+
+    t_metadata data;
+
+    t_key key;
+    t_field * field_table = malloc(sizeof(t_field) * MAXFIELDS);
+    
+    char * t2 = "mot:anagramme 1:anagramme 2:anagramme 3:anagramme 4:anagramme 5:anagramme 6:anagramme 7:anagramme 8:anagramme 9:anagramme 10:anagramme 11:anagramme 12:anagramme 13";
+    char * t3 = "pomme:banane:poire:litchi";
+    split(':', t2, MAXFIELDS, data.key, field_table);
+    data.fieldNames = field_table;
+    // printf("%s", data.fieldNames[2]);
+
+    // split(':', t3, MAXFIELDS, key, field_table);
+    // printf("%p\n", key);
+    // print_fields(field_table);
+    t_tupletable * dico = malloc(sizeof(*dico));
+    split(':', t3, MAXFIELDS, dico->tuples[0].key, dico->tuples[0].value);
+    dico->nbTuples = 1;
+    data.nbFields = 14;
+    // printf("%s", dico->tuples[0].value[3]);
+    print_tuples(data, dico, "pomme");
+}
