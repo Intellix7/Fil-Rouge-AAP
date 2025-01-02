a32339620a69502e99a42992c64d934cfd800fe4
Intellix7 <yac.handsport@gmail.com>
2025-01-02 17:22:28 +0100

    Refactor main function in programs 1 and 2; rename file variable for clarity and improve code formatting

--- programs/program1/main.c
+++ programs/program1/main.c
@@ -1,26 +1,28 @@
 // Include a file that contains all type definitions and functions.
 #include "../definitions.h"
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Halts the program if the user uses the wrong syntax.
-    if (argc < 2) {
+    if (argc < 2)
+    {
         printf("Utilisation :\n./programme <nom du fichier>\n");
         exit(EXIT_FAILURE);
     }
 
     // Opens the datafile.
-    FILE *anagrammeFile = fopen(argv[1], "r");
+    FILE *fileIn = fopen(argv[1], "r");
 
     // STEP 1 : IMPLEMENTING METADATA WITH : SEPARATOR, NB_OF_FIELDS, KEY_NAME, FIELD_NAMES
 
     // Creates the temporary string that will hold the text retrieved from the file.
     char temp[MAX_LEN];
     // STEP 1.1 : SEPARATOR
-    fgets(temp, MAX_LEN, anagrammeFile);
-    while (strcspn(temp, "\n\r") > 1) {
+    fgets(temp, MAX_LEN, fileIn);
+    while (strcspn(temp, "\n\r") > 1)
+    {
         // A line is only read if it contains only one character.
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
     }
 
     // Variable containing the metadatas from the file (SEPARATOR, NB_OF_FIELDS, KEY_NAME, FIELD_NAMES).
@@ -28,11 +30,13 @@ int main(int argc, char **argv) {
     meta.sep = *temp; // Gets separator.
 
     // STEP 1.2 : NB_OF_FIELDS & FIELD_NAMES
-    if (meta.sep != '#') {
+    if (meta.sep != '#')
+    {
         // if the # is not the separator, then it means we have to ignore every line starting with #.
-        fgets(temp, MAX_LEN, anagrammeFile);
-        while (temp[0] == '#') {
-            fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
+        while (temp[0] == '#')
+        {
+            fgets(temp, MAX_LEN, fileIn);
         }
 
         // Retrieves the number of fields.
@@ -41,24 +45,27 @@ int main(int argc, char **argv) {
         // This variable represents a list of fields.
         t_field *field_table = malloc(sizeof(t_field) * meta.nbFields);
 
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
         // Then again, we ignore lines starting with #.
-        while (temp[0] == '#') {
-            fgets(temp, MAX_LEN, anagrammeFile);
+        while (temp[0] == '#')
+        {
+            fgets(temp, MAX_LEN, fileIn);
         }
 
         // Retrieves the fieldNames
         split(meta.sep, temp, meta.nbFields, meta.key, field_table);
         meta.fieldNames = field_table;
-    } else {
+    }
+    else
+    {
         // Case where # is the separator.
         // We then assume that there are no comment line after the separator declaration.
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
 
         // Retrieves the number of fields.
         meta.nbFields = atoi(temp);
 
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
 
         // This variable represents a list of fields
         t_field *field_table = malloc(sizeof(t_field) * meta.nbFields);
@@ -72,23 +79,24 @@ int main(int argc, char **argv) {
     t_tupletable *dico1 = malloc(sizeof(t_tupletable)); // Defines a pointer towards a dictionnary.
     int len_dic = 0;
 
-    while (fgets(temp, MAX_LEN, anagrammeFile) != NULL) {
+    while (fgets(temp, MAX_LEN, fileIn) != NULL)
+    {
         // Reads lines until the end of file.
         split(meta.sep, temp, meta.nbFields, dico1->tuples[len_dic].key, dico1->tuples[len_dic].value);
-        len_dic++; //filling dico1 with its tuples
+        len_dic++; // filling dico1 with its tuples
     }
-    fclose(anagrammeFile);
+    fclose(fileIn);
 
-    dico1->nbTuples = len_dic; //number of keys added in dico1
+    dico1->nbTuples = len_dic; // number of keys added in dico1
 
     char finding[MAX_LEN];
     printf("Mots indexés : %d\n", len_dic);
     printf("Donnez le mot à trouver : \n");
     // printf("%s", finding);
-    while (scanf("%99s", finding) != EOF) {
-        // we can search as many words as we want until we stop the program
+    while (scanf("%99s", finding) != EOF)
+    // we can search as many words as we want until we stop the program
+    {
         print_tuples(meta, dico1, finding);
         printf("\nDonnez le mot à trouver : \n");
     }
-
 }
--- programs/program2/main.c
+++ programs/program2/main.c
@@ -1,8 +1,8 @@
 // Include a file that contains all type definitions and functions.
 #include "../definitions.h"
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Creates the temporary string that will hold the text retrieved from the file.
     char temp[MAX_LEN];
 
@@ -16,21 +16,23 @@ int main(int argc, char **argv) {
     t_hashtable hash;
 
     // Halts the program if the user uses the wrong syntax.
-    if (argc < 3) {
+    if (argc < 3)
+    {
         printf(
             "Utilisation :\n./programme <nom du fichier> <nombre d'alvéoles> <nom de la fonction de hachage (facultatif)>\n");
         exit(EXIT_FAILURE);
     }
 
     // Opens the datafile.
-    FILE *anagrammeFile = fopen(argv[1], "r");
+    FILE *fileIn = fopen(argv[1], "r");
 
     // Stores the number of slots of the hash function
     hash.nbSlots = atoi(argv[2]);
 
     // Sets the hash function to be used, default being first_ASCII
     hash.hashfunction = defaultHash;
-    if (argc == 4) {
+    if (argc == 4)
+    {
         hash.hashfunction = atoi(argv[3]);
     }
     function hashFunctionList[] = {&first_ASCII, &sum_ASCII};
@@ -38,19 +40,22 @@ int main(int argc, char **argv) {
     // STEP 1 : IMPLEMENTING METADATA WITH : SEPARATOR, NB_OF_FIELDS, KEY_NAME, FIELD_NAMES
 
     // STEP 1.1 : SEPARATOR
-    fgets(temp, MAX_LEN, anagrammeFile);
-    while (strcspn(temp, "\n\r") > 1) {
+    fgets(temp, MAX_LEN, fileIn);
+    while (strcspn(temp, "\n\r") > 1)
+    {
         // A line is only read if it contains only one character.
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
     }
     meta.sep = *temp; // Gets separator.
 
     // STEP 1.2 : NB_OF_FIELDS & FIELD_NAMES
-    if (meta.sep != '#') {
+    if (meta.sep != '#')
+    {
         // if the # is not the separator, then it means we have to ignore every line starting with #.
-        fgets(temp, MAX_LEN, anagrammeFile);
-        while (temp[0] == '#') {
-            fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
+        while (temp[0] == '#')
+        {
+            fgets(temp, MAX_LEN, fileIn);
         }
 
         // Retrieves the number of fields.
@@ -59,24 +64,27 @@ int main(int argc, char **argv) {
         // This variable represents a list of fields.
         t_field *field_table = malloc(sizeof(t_field) * meta.nbFields);
 
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
         // Then again, we ignore lines starting with #.
-        while (temp[0] == '#') {
-            fgets(temp, MAX_LEN, anagrammeFile);
+        while (temp[0] == '#')
+        {
+            fgets(temp, MAX_LEN, fileIn);
         }
 
         // Retrieves the fieldNames
         split(meta.sep, temp, meta.nbFields, meta.key, field_table);
         meta.fieldNames = field_table;
-    } else {
+    }
+    else
+    {
         // Case where # is the separator.
         // We then assume that there are no comment line after the separator declaration.
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
 
         // Retrieves the number of fields.
         meta.nbFields = atoi(temp);
 
-        fgets(temp, MAX_LEN, anagrammeFile);
+        fgets(temp, MAX_LEN, fileIn);
 
         // This variable represents a list of fields
         t_field *field_table = malloc(sizeof(t_field) * meta.nbFields);
@@ -93,24 +101,26 @@ int main(int argc, char **argv) {
     int motsIndexes = 0;
     hash.slots = malloc(sizeof(t_list) * hash.nbSlots);
 
-    while (fgets(temp, MAX_LEN, anagrammeFile) != NULL) {
+    while (fgets(temp, MAX_LEN, fileIn) != NULL)
+    {
         t_tuple *tuple = malloc(sizeof(*tuple));
 
         split(meta.sep, temp, meta.nbFields, tuple->key, tuple->value);
         h = hashFunctionList[hash.hashfunction](tuple->key, hash);
-        if (isEmpty(hash.slots[h])) nbCollisions--;
+        if (isEmpty(hash.slots[h]))
+            nbCollisions--;
         hash.slots[h] = addHeadNode(*tuple, hash.slots[h]);
         nbCollisions++;
         motsIndexes++;
     }
 
-
     char finding[MAX_LEN];
     printf("Nombre de mots indexés : %d\nNombre de collisions : %d\n", motsIndexes, nbCollisions);
     printf("Donnez le mot à trouver : \n");
     // printf("%s", finding);
-    while (scanf("%99s", finding) != EOF) {
-        // we can search as many word as we want until we stop the program
+    while (scanf("%99s", finding) != EOF)
+    // we can search as many word as we want until we stop the program
+    {
 
         /* Calls the function print_tuples which handles
         the search of the specified word (finding) and displays the
@@ -120,6 +130,5 @@ int main(int argc, char **argv) {
         printf("\nDonnez le mot à trouver : \n");
     }
 
-
-    fclose(anagrammeFile);
+    fclose(fileIn);
 }
